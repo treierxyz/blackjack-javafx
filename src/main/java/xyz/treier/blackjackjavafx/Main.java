9fb36a00d04f6f314e98b29fc9b10b470e7ba8c6
treierxyz <me@treier.xyz>
2023-05-11 15:08:25 +0300

    Ikoon

--- src/main/java/xyz/treier/blackjackjavafx/Main.java
+++ src/main/java/xyz/treier/blackjackjavafx/Main.java
@@ -2,8 +2,11 @@ package xyz.treier.blackjackjavafx;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Main extends Application {
 
@@ -15,12 +18,13 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage pealava) {
+    public void start(Stage pealava) throws IOException {
         Scene stseen = new Scene(new Pane(),640,480);
         VaateVahetaja.setStseen(stseen);
         VaateVahetaja.vaheta(Vaade.PEAMENÜÜ);
         pealava.setScene(stseen);
         pealava.setTitle("Blackjack");
+        pealava.getIcons().add(new Image(getClass().getResource("icon.png").openStream()));
         pealava.show();
     }
 }
